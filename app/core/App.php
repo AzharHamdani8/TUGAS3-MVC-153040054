a82801638b462350ec096b0055d8f71586c90c28
azharhamdani8 <azhar.153040054@mail.unpas.ac.id>
2019-10-03 11:16:41 +0700

    membuat routing  dan berhasil mengubah url yang di tulis menjadi elemen pada array

--- app/core/App.php
+++ app/core/App.php
@@ -4,8 +4,21 @@
  	public function __construct()
 
  	{
+ 	
+ 		$url = $this->parseURL();
+ 		var_dump($url);
+ 	}
 
- 		echo 'OK';
- }
+ 	public function parseURL()
+ 	{
+ 		if( isset($_GET['url']) ) {
+ 			$url = rtrim($_GET['url'],'/');
+ 			$url = filter_var($url, FILTER_SANITIZE_URL);
+ 			$url = explode('/', $url);
+ 			return $url;
+ 		}
+ 	}
+
+ 	
 
 }
