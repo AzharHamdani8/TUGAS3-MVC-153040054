1d47c810006371ed14e11fc1919c96ab73e4459b
azharhamdani8 <azhar.153040054@mail.unpas.ac.id>
2019-10-03 22:15:23 +0700

    membuat file controller dan isinya

---
+++ app/controllers/About.php
@@ -0,0 +1,16 @@
+<?php 
+
+class About {
+
+	public function index($nama = 'Azhar', $pekerjaan = 'mahasiswa')
+	{
+		echo "Hallo, nama saya $nama, saya adalah seorang $pekerjaan";
+	}
+
+	public function page()
+	{
+		echo 'About/page';
+	}
+}
+
+ ?>
