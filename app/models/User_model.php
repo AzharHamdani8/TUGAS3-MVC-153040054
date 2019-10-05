97af9800054b33d5c1aed94434611eed89e261ee
azharhamdani8 <azhar.153040054@mail.unpas.ac.id>
2019-10-06 00:06:16 +0700

    membuat model dan mengambil data dari data base

---
+++ app/models/User_model.php
@@ -0,0 +1,14 @@
+<?php 
+class User_model {
+	private $nama = 'azhar';
+
+	public function getUser() 
+	{
+
+	return $this->nama;
+	
+	}
+}
+
+
+ ?>
---
+++ app/views/mahasiswa/index.php
@@ -0,0 +1,28 @@
+<div class="container mt-5">
+	
+	<div class="row">
+		
+		<div class="col-6">
+			
+			<h3>DAFTAR MAHASISWA</h3>
+
+			<?php foreach( $data['mhs'] as $mhs)  : ?>
+
+				<ul>
+					
+					<li><?= $mhs['nama'];?></li>
+					<li><?= $mhs['nrp'];?></li>
+					<li><?= $mhs['email'];?></li>
+					<li><?= $mhs['jurusan'];?></li>
+				</ul>
+
+
+			<?php endforeach;?>
+		</div>
+
+
+	</div>
+
+
+
+</div>
